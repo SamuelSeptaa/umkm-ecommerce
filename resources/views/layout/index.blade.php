999c086d3c60ee8030272237f92c394360db5022
SamuelSeptaa <samuelseptaa@gmail.com>
2023-07-18 14:21:43 +0700

    metakeyword

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Ogani Template">
-    <meta name="keywords" content="Ogani, unica, creative, html">
+    <meta name="description" content="MarketPlace website for UMKM Palangka Raya">
+    <meta name="keywords" content="Marketplace, UMKM, Palangka Raya">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
